09853b52767934979f289b691f683461ab408402
rickydalziel <rickyd493@hotmail.com>
2014-08-21 17:57:01 +0100

    setup teaspoon so that rake teaspoon is run, not picking up spec files though

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -2,6 +2,10 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( teaspoon.css )
+Rails.application.config.assets.precompile += %w( jasmine/1.3.1.js )
+Rails.application.config.assets.precompile += %w( teaspoon-jasmine.js )
+
 # Rails.application.config.assets.precompile += %w{ /feedback_mailer/application.css }
 # Rails.application.config.assets.precompile += %w{ application.js }
 # Rails.application.config.assets.paths << Root.join("app", "assets", "images")
