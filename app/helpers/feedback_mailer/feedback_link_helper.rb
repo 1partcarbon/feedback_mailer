2d3e3d3b5f9c32f67cf6fdebbcd05568cb234c2e
rickydalziel <rickyd493@hotmail.com>
2014-08-22 10:45:32 +0100

    updated the feedback link helper to render a partial with everything in it now.

--- app/helpers/feedback_mailer/feedback_link_helper.rb
+++ app/helpers/feedback_mailer/feedback_link_helper.rb
@@ -1,9 +1,7 @@
 module FeedbackMailer
 	module FeedbackLinkHelper
 		def feedback_link
-      content_tag :div, :class => "feedback_container" do
-        link_to(t('feedback_mailer.feedback'), feedback_mailer.new_feedback_path, class: "feedback_link" )
-      end
+      render partial: 'feedback_mailer/feedback_view'
 		end
 	end
 end
--- spec/javascripts/feedback_spec.js.coffee
+++ spec/javascripts/feedback_spec.js.coffee
@@ -4,4 +4,4 @@
 describe 'feedback', ->
   describe 'clicking the feedback link', ->
     it 'displays the feedback form', ->
-   expect( $('.feedback_form_container')).toHaveClass('open')
+     expect( $('.feedback_form_container')).toHaveClass('open')
